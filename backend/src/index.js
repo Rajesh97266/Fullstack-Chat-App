41d322975df0be7cbab19198ff7b734c1a675067
Rajesh97266 <rajesh97266@gmail.com>
2025-02-08 13:47:30 +0530

    auth-routes-created

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,20 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-const PORT  = process.env.PORT || 5001
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT , ()=> {
-    console.log("Server is running on port on " + PORT);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port on " + PORT);
+  connectDB();
+});
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Mongo DB connected" + connection.connection.host);
+    console.log("✅ Mongo DB connected connectDB" + connection.connection.host);
   } catch (error) {
     console.log(error);
     process.exit(1);
